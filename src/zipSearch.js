215bf6d40d7bfb9a96611fbcc3eb2ed48537e2cb
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-05 11:49:31 -0400

    get zipcode for search

--- src/zipSearch.js
+++ src/zipSearch.js
@@ -6,6 +6,29 @@ class ZipSearch extends Component {
 
   constructor(props){
     super(props)
+
+    this.state = {
+      data: [
+          {
+            zip: '11201',
+            city: 'brooklyn',
+            state: 'NY',
+            location: '(40.71, -73.99)',
+            popEstimate: '1234567',
+            totalWages: '88888654'
+        }
+      ]
+    }
+  }
+
+  onChangeHandle = (event) => {
+
+      if (event.target.value.length === 5){
+        console.log(event.target.value);
+        //fecth data 
+      } else {
+          console.log("Not A Zipcode");
+      }
   }
 
   render(){
@@ -15,7 +38,7 @@ class ZipSearch extends Component {
             <form>
               <label>
                   Zip Code
-                  <input name="zipField" type="text" placeholder="try 11201"/>
+                  <input name="zipField" value={this.state.zip} type="text" placeholder="try 11201" onChange={this.onChangeHandle}/>
               </label>
             </form>
           </div>
