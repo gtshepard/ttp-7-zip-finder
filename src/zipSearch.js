cc630ae313df68f15f2140ed7c5eb29cc3c49f66
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-05 13:17:14 -0400

    retrieve zipcode

--- src/zipSearch.js
+++ src/zipSearch.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import './zipSearch.css'
-
+import axios from 'axios'
 
 class ZipSearch extends Component {
 
@@ -10,7 +10,7 @@ class ZipSearch extends Component {
     this.state = {
       data: [
           {
-            zip: '11201',
+            id: '11201',
             city: 'brooklyn',
             state: 'NY',
             location: '(40.71, -73.99)',
@@ -21,11 +21,27 @@ class ZipSearch extends Component {
     }
   }
 
+  async fetchZipData(zip){
+
+    try {
+      let {data} = await axios.get('http://ctp-zip-api.herokuapp.com/zip/' + zip);
+
+      data.forEach(function(element){
+          console.log(element);
+      });
+
+    } catch(err){
+      console.log(err);
+    }
+  }
+
   onChangeHandle = (event) => {
 
       if (event.target.value.length === 5){
         console.log(event.target.value);
-        //fecth data 
+        //fecth data
+        this.fetchZipData(event.target.value);
+
       } else {
           console.log("Not A Zipcode");
       }
@@ -38,7 +54,7 @@ class ZipSearch extends Component {
             <form>
               <label>
                   Zip Code
-                  <input name="zipField" value={this.state.zip} type="text" placeholder="try 11201" onChange={this.onChangeHandle}/>
+                  <input name="zipField" value={this.state.id} type="text" placeholder="try 11201" onChange={this.onChangeHandle}/>
               </label>
             </form>
           </div>
