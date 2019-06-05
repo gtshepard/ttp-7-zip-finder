2ab07e6896b6428f7e145706bec475e1e8fed45b
Garrison Shepard <shepard.garrisont@gmail.com>
2019-06-05 10:53:12 -0400

    add nav bar

---
+++ src/nav.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+const Nav = () => {
+
+  return (
+    <div className="nav">
+      <div className="nav-header">
+        <h2> Zip Finder</h2>
+      </div>
+    </div>
+  )
+}
+export default Nav;
